710f9bef9f36298b849ece37c7dd5c9676183257
MD NOORUL NABI ANSARI <noor.csarchitect@gmail.com>
2025-12-15 01:11:18 +0400

    Update main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import DB "github.com/nurulnabi/go-finsight/internal/database"
 func main() {
 
 	var cfg DB.DBConfig = DB.SQLConfig{
-		DB_URI: "postgresql://finsight:hAwJQHsgdaBvUjSGm9zlfw@finsight-19064.j77.aws-ap-south-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
+		DB_URI: "",
 	}
 	arr := []DB.DBConfig{cfg}
 
